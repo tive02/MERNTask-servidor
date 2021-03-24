dad0910aa5f326ed2b3eac74fab4258a4978d717
Jorge Moreno <jsmorenop@unal.edu.co>
2021-03-24 11:05:48 -0500

    Creando el servidor express

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("DEsde el index,js");
+const express = require("express");
+
+//crear el servidor
+const app = express();
+//Puerto de la APP
+const PORT = process.env.PORT || 4000;
+//Arrancar la APP
+app.listen(PORT, () => {
+  console.log(`El servidor Esta funcionado en el puerto ${PORT}`);
+});
