8b704eb247ebc0674d7689f6365425b96cc5df15
Jorge Moreno <jsmorenop@unal.edu.co>
2021-03-26 17:07:37 -0500

    Configurando Rutas de Usuario

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const app = express();
 conectarDB();
 //Puerto de la APP
 const PORT = process.env.PORT || 4000;
+
+//Importar las rutas
+app.use("/api/usuarios", require("./routes/usuarios"));
+
 //Arrancar la APP
 app.listen(PORT, () => {
   console.log(`El servidor Esta funcionado en el puerto ${PORT}`);
---
+++ routes/usuarios.js
@@ -0,0 +1,9 @@
+//Rutas para Crear los usuarios
+const express = require("express");
+const router = express.Router();
+
+//Crea un usuario
+//Enpoint  = api/usuarios
+router.post("/", () => {});
+
+module.exports = router;
