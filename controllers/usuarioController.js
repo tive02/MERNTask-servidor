a1c87c807d4252b2ba32c23564f43db277bc0eda
Jorge Moreno <jsmorenop@unal.edu.co>
2021-03-26 21:15:32 -0500

    Revisando si el usuario ha sido registrado anteriormente

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,13 +1,19 @@
 const Usuario = require("../models/Usuario");
 exports.crearUsuario = async (req, res) => {
+  //Extraer Email y password
+  const { email, password } = req.body;
   try {
-    let usuario;
+    //Revisar que el usuario sea unico
+    let usuario = await Usuario.findOne({ email });
+    if (usuario) {
+      return res.status(400).json({ msg: "El usuario ya existe" });
+    }
     // crea el  nuevo usuario
     usuario = new Usuario(req.body);
     //guardar usuario
     await usuario.save();
     //Mensaje de confirmacion
-    res.send("Usuario Creado Correctamente");
+    res.json({ msg: "Usuario creado correctamente" });
   } catch (error) {
     console.log(error);
     res.status(400).send("Existio un error");
