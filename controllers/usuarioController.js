25edbd9a80a9d0678b296de038ba5d763b6d7691
Jorge Moreno <jsmorenop@unal.edu.co>
2021-03-26 19:41:39 -0500

    Insertando Usuarios al BD

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,3 +1,15 @@
-exports.crearUsuario = (req, res) => {
-  console.log(req.body);
+const Usuario = require("../models/Usuario");
+exports.crearUsuario = async (req, res) => {
+  try {
+    let usuario;
+    // crea el  nuevo usuario
+    usuario = new Usuario(req.body);
+    //guardar usuario
+    await usuario.save();
+    //Mensaje de confirmacion
+    res.send("Usuario Creado Correctamente");
+  } catch (error) {
+    console.log(error);
+    res.status(400).send("Existio un error");
+  }
 };
