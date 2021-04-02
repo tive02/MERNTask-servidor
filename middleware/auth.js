e7f81f8aa993b711b1c93e02955bb6a66a1d76c2
Jorge Moreno <jsmorenop@unal.edu.co>
2021-04-02 10:45:48 -0500

    Eliminando la impresion del token en consola

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,7 +3,6 @@ const jwt = require("jsonwebtoken");
 module.exports = function (req, res, next) {
   //Leer el token del header
   const token = req.header("x-auth-token");
-  console.log(token);
 
   //Revisar si no hay token
   if (!token) {
